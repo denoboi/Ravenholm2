560980c6c1a39ef79beaf7eb7eeb48ca39caaf2c
Deniz Kaymakci <deniz.kaymakci@hotmail.com>
2024-04-19 19:12:45 +0300

    added headbob + hit particle

--- Assets/[Game]/Scripts/Weapon.cs
+++ Assets/[Game]/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,71 +9,58 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera fpCamera;
     [SerializeField] Camera muzzleCam;
     [SerializeField] float damage = 20f;
-   
+
     [SerializeField] ParticleSystem dash;
-    
+    [SerializeField] GameObject hitFx;
+
     [SerializeField] GameObject fireball;
     [SerializeField] float shootForce = 5f;
 
-    [SerializeField] public Vector3 smoothAds;
-    
-    [SerializeField] public Ammo ammoSlot;
+    public Vector3 smoothAds;
+
+    public Ammo ammoSlot;
     public bool ammoAvailable = true;
-    bool isAiming = false;
+    private bool isAiming = false;
     public Vector3 aimDownSight;
     public Vector3 hipFire;
-    
-     
-    [SerializeField]
-    float aimspeed = 1500f;
-    
-    void Start()
-    {
-        
-    }
 
-    
+
+    [SerializeField]
+    private float aimspeed = 1500f;
+
+
+
     void Update()
     {
-         if(Input.GetMouseButtonDown(0))       //if (Input.GetButtonDown("Fire1"))
+        if (Input.GetMouseButtonDown(0))
         {
             Shoot();
-           
-
-            //fireball.Play();
-            GameObject projectile = (GameObject)Instantiate(fireball, transform.position, transform.rotation);
-            projectile.GetComponent<Rigidbody>().AddForce(fireball.transform.forward * shootForce); 
-            
         }
 
-       
 
-        if(Input.GetMouseButtonDown(1))
+
+        if (Input.GetMouseButtonDown(1))
         {
-
             smoothAds = transform.localPosition;
 
             isAiming = true;
-        }    
+        }
         if (Input.GetMouseButton(1))
         //0, 0.17,0.235
         {
             transform.localPosition = Vector3.Lerp(transform.localPosition, aimDownSight, aimspeed * Time.deltaTime);
-            
-            
         }
         if (Input.GetMouseButtonUp(1))
         {
             isAiming = false;
-            
         }
-        if(isAiming == false && transform.localPosition != hipFire)
+        if (isAiming == false && transform.localPosition != hipFire)
         {
             transform.localPosition = Vector3.Lerp(transform.localPosition, hipFire, 7f * Time.deltaTime);
         }
     }
 
-    void Shoot()
+    private void Shoot()
     {
         if (ammoSlot.GetCurrentAmmo() > 0)
         {
@@ -84,36 +72,39 @@ public class Weapon : MonoBehaviour
             ProcessRaycast();
             dash.Play();
             ammoSlot.ReduceAmmo();
-                     
         }
 
-        if (ammoSlot.GetCurrentAmmo() <= 0 )
+        if (ammoSlot.GetCurrentAmmo() <= 0)
         {
             ammoAvailable = false;
         }
 
-        
+
 
     }
-        
-     
+
+
     public void ProcessRaycast()
     {
         RaycastHit hit;
         if (Physics.Raycast(muzzleCam.transform.position, muzzleCam.transform.forward, out hit, range))
         {
             Debug.Log("I hit this thing:" + hit.transform.name);
+            CreateHitFX(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-            
+
+
             if (target == null) return; //enemy yerine diger objelere ates edersem.
-            
+
             target.TakeDamage(damage); // asil oldurecek olan bu.
-            
+
         }
-       
+
     }
 
-  
-
-        
+    private void CreateHitFX(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitFx, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 2);
+    }
 }
